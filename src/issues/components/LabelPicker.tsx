6842dde88cdeba90bd875bdd2c984892fc309433
fanovoa <fabian93novoa@gmail.com>
2023-07-26 16:55:02 -0500

    Configuracion de axios y tipo de datos Label

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,5 +1,22 @@
+import { useQuery } from '@tanstack/react-query';
+import { githubApi } from '../../api/githubApi';
+import { Label } from '../interfaces/label';
+
+const getLabels = async():Promise<Label[]> => {
+
+  const { data } =await githubApi.get<Label[]>('/labels');
+  return data;
+}
 
 export const LabelPicker = () => {
+
+  const labelsQuery = useQuery(
+    ['labels'],
+    getLabels
+  )
+
+
+
   return (
     <div>
         <span 
